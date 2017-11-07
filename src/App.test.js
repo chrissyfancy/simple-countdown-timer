ac225d0c85b193576d055708de1585b677ae06eb
Chrissy Fancy <chrissyfancy@gmail.com>
2017-11-07 17:16:08 -0500

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+console.error = message => {
+   throw new Error(message);
+};
+
+it('should render the App component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should render the initial state', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state()).toEqual({ startTime: 0, endTime: 0, countdownTimer: 0, errors: [] })
+});
+
+it('should include the following text', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.text()).toEqual("Simple Countdown TimerStart TimeEnd TimeStart CountdownSeconds Remaining:0")
+});
+
+it('should have the submit button', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('button')).toHaveLength(1);
 });
